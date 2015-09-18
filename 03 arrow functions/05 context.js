cab384c7b79dc55efd44cd64a0f6f96359fe4448
JD Isaacks <jd@jisaacks.com>
2015-09-17 23:49:44 -0400

    update example

--- 03 arrow functions/05 context.js
+++ 03 arrow functions/05 context.js
@@ -1,17 +1,17 @@
-let numbers = [1, 2, 3]
+let numbers = [1,2];
 let obj = {};
 
 obj.unbound = function() {
   let that = this;
   numbers.forEach(function(n) {
-    console.log('unbound:', this === that);
+    console.log('unbound:', n, this === that);
   });
 };
 
 obj.bound = function() {
   let that = this;
   numbers.forEach(n => {
-    console.log('bound:', this === that);
+    console.log('bound:', n, this === that);
   });
 };
 
